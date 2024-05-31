73be09deb3045376c2c18bbcd7a11c53bacfcc59
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-05-31 01:12:10 -0300

    entrega sets.py

--- sets.py
+++ sets.py
@@ -1,17 +1,15 @@
-def enumerate_list(strings):
-    result = []
-    count = 0 
-    for string in strings:
-        if string: 
-            result.append(f"{count}. {string}")
-            count += 1
-    return result
-def enumerate_backwards(strings):
-    result = []
-    count = 0  
-    for string in strings:
-        if string: 
-            reversed_string = string[::-1]
-            result.append(f"{count}. {reversed_string}")
-            count += 1
-    return result
+from sets_categories_data import (ALCOHOLS)
+
+def clean_ingredients(dish_name, dish_ingredients):
+    dish_ing1 = set(dish_ingredients)
+    return(dish_name, dish_ing1)
+
+
+def check_drinks(drink_name, drink_ingredients):
+    drin_ing2 = set(drink_ingredients)
+    alcohol_1 = set(ALCOHOLS)
+
+    if drin_ing2.intersection(alcohol_1) == set():
+        return (str(drink_name) + " " + "Mocktail")
+    else:
+        return (str(drink_name) + " " + "Cocktail")
