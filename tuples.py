c7e43c5fb1c9d74dea0a3eb681a847b1d941fb04
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-05-30 22:21:31 -0300

    entregas tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,16 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
+    coordinate = record
+    return coordinate
 
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+    y = (coordinate[0], coordinate[1])
+    return y
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    az_tesoro, az_coordenada = azara_record
+    rui_ubicacion, rui_coordenada, rui_cuadrante = rui_record
+    if az_coordenada[0] != rui_coordenada[0] or az_coordenada[1] != rui_coordenada[1]:
+        return "not a match"
+    return {(az_tesoro, az_coordenada, rui_coordenada, rui_cuadrante)}
