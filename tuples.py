57287b6e472f4f2aa3215796ff6aec64b2da9ad0
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-05-31 01:08:51 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,16 +1,29 @@
-def get_coordinate(record):
-    coordinate = record
-    return coordinate
+def index_of_by_index(word, lst, start_index):
+    for index, element in enumerate(lst[start_index:], start_index):
+        if element == word:
+            return index
+    return -1
+def index_of_empty(lst):
+    for index, element in enumerate(lst):
+        if element == "":
+            return index
+    return -1
+def index_of(word, lst):
+    for index, element in enumerate(lst):
+        if element == word:
+            return index
+    return -1
+def put(word, lst):
+    for index, element in enumerate(lst):
+        if element == "":
+            lst[index] = word
+            return index
+    return -1
 
-
-def convert_coordinate(coordinate):
-    y = (coordinate[0], coordinate[1])
-    return y
-
-
-def create_record(azara_record, rui_record):
-    az_tesoro, az_coordenada = azara_record
-    rui_ubicacion, rui_coordenada, rui_cuadrante = rui_record
-    if az_coordenada[0] != rui_coordenada[0] or az_coordenada[1] != rui_coordenada[1]:
-        return "not a match"
-    return {(az_tesoro, az_coordenada, rui_coordenada, rui_cuadrante)}
+def remove(word, lst):
+    count = 0
+    for index, element in enumerate(lst):
+        if element == word:
+            lst[index] = ""
+            count += 1
+    return count
