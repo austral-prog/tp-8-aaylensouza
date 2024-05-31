d110a32dcd8d1a13195ea511cd5dd903509ece4a
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-05-31 01:14:40 -0300

    entrega tuples.py

--- tuples.py
+++ tuples.py
@@ -1,29 +1,23 @@
-def index_of_by_index(word, lst, start_index):
-    for index, element in enumerate(lst[start_index:], start_index):
-        if element == word:
-            return index
-    return -1
-def index_of_empty(lst):
-    for index, element in enumerate(lst):
-        if element == "":
-            return index
-    return -1
-def index_of(word, lst):
-    for index, element in enumerate(lst):
-        if element == word:
-            return index
-    return -1
-def put(word, lst):
-    for index, element in enumerate(lst):
-        if element == "":
-            lst[index] = word
-            return index
-    return -1
+def get_coordinate(record):
+    return record [-1]
 
-def remove(word, lst):
-    count = 0
-    for index, element in enumerate(lst):
-        if element == word:
-            lst[index] = ""
-            count += 1
-    return count
+def convert_coordinate(coordinate):
+    x = coordinate [0]
+    y = coordinate [1]
+    return (f"{x}", f"{y}")
+
+
+def create_record(azara_record, rui_record):
+    coordenada_1 = azara_record [-1]
+    x1 = coordenada_1 [0]
+    y1 = coordenada_1 [-1]
+
+    coordenada_2 = rui_record [-2]
+    x2 = coordenada_2 [0]
+    y2 = coordenada_2 [-1]
+    
+    if x1 == x2 and y1 == y2:
+        return azara_record + rui_record
+
+    else:
+        return "not a match"
